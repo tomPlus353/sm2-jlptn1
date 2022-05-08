3cfca10a404a87cb2e990b1e0031f78895ae2c7c
Tomas Sullivan <tsulliva@tcd.ie>
2022-05-08 14:26:40 +0900

    add eloquent orm models

---
+++ Models/card.py
@@ -0,0 +1,23 @@
+from config import *
+import sentence
+from orator.orm import has_many
+
+class Card(Model):
+
+    @has_many
+    def sentences(self):
+        Sentence = sentence.Sentence
+        return Sentence
+Sentence = sentence.Sentence
+testCard = Card.join("sentences", "sentences.card_id", "=", "cards.id") \
+.where("sentences.id", "=", "5000" ) \
+.get()
+
+for card in testCard:
+    print(card.kanji)
+    print(card.sentence)
+    print(card.kana)
+    print(card.definition)
+print(testCard.count())
+
+#.where("sentences.sentence", "LIKE", "%あ%" ) \
