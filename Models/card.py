cd800becda55cc61231001bc910e865820acf679
Tomas Sullivan <mossysendai@gmail.com>
2022-07-17 00:57:08 +0900

    finished adding example sentences. Added method to tell if card has one sucessful(higher quality than 4) review. Based on this English definition can be hidden, and review can be speeded up with only one answer correct in row needed to finish a card in each section(if they get it wrong then back to requiring two successful answers in a row)

--- Models/card.py
+++ Models/card.py
@@ -12,9 +12,19 @@ class Card(Model):
     __timestamps__ = False
 
     def initializeWrongCounts(self):
-       self.wrongRead = 0
-       self.wrongWrite = 0
-       self.wrongMean = 0
+       #when studying a card for the second time, user gets chance to pass with just one correct answer for each category
+       if self.hasOneSuccessfulReview():
+          self.wrongRead = 1
+          self.wrongWrite = 1
+          self.wrongMean = 1
+      #when studing for the first time 2 correct answers in a row are always required
+       else:
+          self.wrongRead = 0
+          self.wrongWrite = 0
+          self.wrongMean = 0
+    
+    def hasOneSuccessfulReview(self):
+       return self.repetitions >= 1
     
     def setPerfectQuiz(self):
        self.reading_score = 2
@@ -25,6 +35,10 @@ class Card(Model):
        self.wrongWrite = 0
        self.wrongMean = 0
        #self.due_date = date.today()
+   
+    def partWrittenKanji(self):
+       #return character
+       return ''.join([charKanji if charKanji not in self.kana else '' for charKanji in self.kanji])
 
     def calculateAverageQuality(self):
        #start with max quality if 5 and subtract two points for each wrong answer
